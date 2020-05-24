919d625e3a13cc4fb567741d730b35c95679285b
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-24 02:44:09 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,36 @@
 class RecipesController < ApplicationController
 
+    get '/recipes' do
+        if is_logged_in?
+            @user = current_user
+            @recipes = Recipe.all
+            erb :'recipes/recipes'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/recipes/new' do
+        if is_logged_in?
+            @user = current_user
+            erb :'recipes/create_recipe'
+        else
+            redirect '/login'
+        end
+    end
+
+    post '/recipes' do
+        @user = current_user
+        # @tweet = Tweet.create(content: params[:content], user: @user)
+        if !params[:content].empty?
+            @recipe = Recipe.create(:name=>params[:name], :content=>params[:content], user: @user)
+            @user.recipes << @recipe
+            @user.save
+
+            redirect to "/recipes"
+        else
+            redirect to 'recipes/new'
+        end
+    end
 
 end
