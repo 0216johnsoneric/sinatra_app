cca095f04731c61e171d1476a541a441a2bc6a53
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-27 20:38:00 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class RecipesController < ApplicationController
 
     get '/recipes' do
@@ -21,16 +22,80 @@ class RecipesController < ApplicationController
 
     post '/recipes' do
         @user = current_user
-        # @tweet = Tweet.create(content: params[:content], user: @user)
         if !params[:content].empty?
             @recipe = Recipe.create(:name=>params[:name], :content=>params[:content], user: @user)
             @user.recipes << @recipe
             @user.save
-
             redirect to "/recipes"
         else
             redirect to 'recipes/new'
         end
     end
 
+    get '/recipes/:id' do
+        if is_logged_in?
+            @user = current_user
+            @recipe = Recipe.find_by_id(params[:id])
+            erb :'/recipes/show_recipe'
+        else
+            redirect to '/login'
+        end
+    end
+
+    # get '/recipes/:id/edit' do
+    #     if is_logged_in?
+    #     @user = current_user
+    #     @recipe = Recipe.find_by_id(params[:id])
+    #         if @recipe.user 
+    #         # binding.pry
+    #             erb :"recipes/edit_recipe"
+    #         else
+    #             redirect to '/recipes'
+    #          end
+    #     else
+    #         redirect to '/recipes'
+    #     end
+    # end
 end
+
+    # get '/recipes/:id/edit' do
+    #     @recipe = Recipe.find_by_id(params[:id])
+    #     if is_logged_in? && @recipe.user == current_user
+    #         erb :'recipes/edit_recipe'
+    #     else
+    #         redirect '/login'
+    #     end
+    # end
+
+
+    # patch '/recipes/:id/edit' do
+    #     @recipe = Recipe.find_by_id(params[:id])
+    #     @user = current_user
+    #     if !params[:content].empty? 
+    #         @recipe.update(:content => params[:content])
+    #         @recipe.save
+    #         redirect "recipes/#{@recipe[:id]}"
+    #     else 
+    #         redirect "recipes/#{@recipe[:id]}/edit"
+    #     end
+    # end
+
+
+
+#     patch '/recipes/:id/edit' do
+#         @recipe = Recipe.find_by(id: params[:id])
+#         @recipe.name = params[:cause][:name]
+#         @cause.description = params[:cause][:description]
+#         @cause.funding = params[:cause][:funding]
+#         if params[:cause][:category_id]
+#       @cause.category_id = params[:cause][:category_id]
+#     else
+#       @cause.category = Category.find_or_create_by(name: params[:new_category].capitalize)
+#     end
+
+#     if @cause.save
+#       redirect to "/cause/#{@cause.id}"
+#     else
+#       erb :"causes/show", locals: {message: "The cause wasn't updated."}
+#     end
+#   end
