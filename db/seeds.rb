2e82721e903d9f5074f9339b57795cee21c1d296
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-31 23:24:44 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+eric = User.create(:username => "ericjohnson0216", :email => "ericjohnson0216@icloud.com", :password => "Hello")
+fruit_smoothie = Recipe.create(:name => "Banana Fruit Smoothie", :content => "In a blender, combine pineapple juice, bananas, honey and ice!", :item => "Pineapple Juice", :quantity => 1, :unit => "Cups", :item => "Bananas", :quantity => 3)
+# fruit_smoothie.Recipe.create(:item => "Pineapple Juice", :quantity => 1, :unit => "Cup")
+# fruit_smoothie.Recipe.create(:item => "Bananas", :quantity => 3)
+# fruit_smoothie.Recipe.create(:item => "Honey", :quantity => 1, :unit => "Tablespoon")
+# fruit_smoothie.Recipe.create(:item => "Ice", :quantity => 2, :unit => "Cups")
+# fruit_smoothie.creator = eric
+fruit_smoothie.save
+
